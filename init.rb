a87072a30a4b1f2a2690fb7f44182c088f694986
Douglas Soares de Andrade <contato@douglasandrade.com>
2012-05-16 02:43:00 -0300

    Updating the plugin to run on redmine 2.0 =)

--- init.rb
+++ init.rb
@@ -1,20 +1,17 @@
+require 'issue'
 require 'redmine'
-require 'dispatcher'
 
-Dispatcher.to_prepare :redmine_auto_percent do
-  require_dependency 'issue'
-  # Guards against including the module multiple time (like in tests)
-  # and registering multiple callbacks
-  unless Issue.included_modules.include? RedmineAutoPercent::IssuePatch
-    Issue.send(:include, RedmineAutoPercent::IssuePatch)
-  end
+require File.dirname(__FILE__) + '/lib/issue_patch'
+
+unless Issue.included_modules.include? RedmineAutoPercent::IssuePatch
+  Issue.send(:include, RedmineAutoPercent::IssuePatch)
 end
 
 Redmine::Plugin.register :redmine_auto_percent do
   name 'Redmine Auto Done 100%'
-  author 'Wade Womersley'
+  author 'Wade Womersley and EBC developers at github'
   description 'Automatically sets 100% done on Resolved or Closed'
-  version '0.0.1'
+  version '0.1.0'
   url ''
-  author_url 'http://www.xcitestudios.com/'
+  author_url 'http://github.com/ebc/'
 end
